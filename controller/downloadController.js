2b72c8b3f50f58c2e064334d0ea45eab5e744202
MagicSoft <magicsoft.uz@gmail.com>
2022-07-15 11:42:44 +0500

    Add files

--- controller/downloadController.js
+++ controller/downloadController.js
@@ -3,6 +3,12 @@ const File = require("../models/fileModel");
 const download = async (req, res) => {
   const file = await File.findOne({ uuid: req.params.uuid });
   const respone = `${__dirname}/../${file.path}`;
+  console.log(file.filename);
+  changeName = "salom";
+  res.setHeader(
+    "Content-Disposition",
+    'attachment; filename="MyFile_XYZT.doc"'
+  );
   res.download(respone);
 };
 
--- controller/fileController.js
+++ controller/fileController.js
@@ -11,7 +11,7 @@ const storage = multer.diskStorage({
   filename: (req, file, cb) => {
     const uniqueName = `${
       Date.now() - Math.round(Math.random() * 1e9)
-    }${path.extname(file.originalname)}`;
+    }-${path.basename(file.originalname)}`;
     cb(null, uniqueName);
   },
 });
